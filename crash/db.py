cb9ebc73bcbe29368bde461d3af5bd13757cfa94
zzzzer <zzzzer91@gmail.com>
2019-05-31 14:36:26 +0800

    fx

--- crash/db.py
+++ crash/db.py
@@ -14,9 +14,7 @@ def insert_data(mysql_config: MysqlConfig, sql: str, item: Dict) -> int:
     """插入数据。"""
 
     mysql_conn = pymysql.connect(
-        host=mysql_config['host'], port=mysql_config['port'],
-        user=mysql_config['user'], password=mysql_config['pwd'],
-        db=mysql_config['db'], autocommit=True
+        **mysql_config
     )
     cursor = mysql_conn.cursor()
 
@@ -48,9 +46,7 @@ def truncate_table(mysql_config: MysqlConfig, table: str) -> None:
     """清空指定表。"""
 
     mysql_conn = pymysql.connect(
-        host=mysql_config['host'], port=mysql_config['port'],
-        user=mysql_config['user'], password=mysql_config['pwd'],
-        db=mysql_config['db']
+        **mysql_config
     )
     cursor = mysql_conn.cursor()
 
